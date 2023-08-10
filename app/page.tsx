00060b5faf03c360cf7a7509ec06fa13faacd161
Nélson <nm.matos@campus.fct.unl.pt>
2023-08-10 18:47:58 +0100

    Fixed things

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,37 @@
 'use client';
 
 import FormSection from '@/components/sections/FormSection';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className=" bg-white">
       <div className=" flex justify-between items-center container mt-20 mb-20">
-        <img src="./images/image.png" alt="No image" />
+        <Image src="./images/image.png" alt="No image" />
         <div className=" w-full h-full px-10">
           <FormSection />
         </div>
       </div>
       <div className=" flex justify-between items-center container mt-20 mb-20">
-        <img src="./images/image.png" alt="No image" />
+        <Image src="./images/image.png" alt="No image" />
         <div className=" w-full h-full px-10">
           <FormSection />
         </div>
       </div>
       <div className=" flex justify-between items-center container mt-20 mb-20">
-        <img src="./images/image.png" alt="No image" />
+        <Image src="./images/image.png" alt="No image" />
         <div className=" w-full h-full px-10">
           <FormSection />
         </div>
       </div>
       <div className=" flex justify-between items-center container mt-20 mb-20">
-        <img src="./images/image.png" alt="No image" />
+        <Image src="./images/image.png" alt="No image" />
         <div className=" w-full h-full px-10">
           <FormSection />
         </div>
       </div>
       <div className=" flex justify-between items-center container mt-20 mb-20">
-        <img src="./images/image.png" alt="No image" />
+        <Image src="./images/image.png" alt="No image" />
         <div className=" w-full h-full px-10">
           <FormSection />
         </div>
