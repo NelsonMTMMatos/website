fca3524f88b06d90e478693aba4898c94d0affa5
Nélson <nm.matos@campus.fct.unl.pt>
2023-08-10 18:57:51 +0100

    Fixed things

--- app/page.tsx
+++ app/page.tsx
@@ -7,31 +7,56 @@ export default function Home() {
   return (
     <main className=" bg-white">
       <div className=" flex justify-between items-center container mt-20 mb-20">
-        <Image src="./images/image.png" alt="No image" />
+        <Image
+          src="/images/image.png"
+          alt="No image"
+          width={600}
+          height={300}
+        />
         <div className=" w-full h-full px-10">
           <FormSection />
         </div>
       </div>
       <div className=" flex justify-between items-center container mt-20 mb-20">
-        <Image src="./images/image.png" alt="No image" />
+        <Image
+          src="/images/image.png"
+          alt="No image"
+          width={600}
+          height={300}
+        />
         <div className=" w-full h-full px-10">
           <FormSection />
         </div>
       </div>
       <div className=" flex justify-between items-center container mt-20 mb-20">
-        <Image src="./images/image.png" alt="No image" />
+        <Image
+          src="/images/image.png"
+          alt="No image"
+          width={600}
+          height={300}
+        />
         <div className=" w-full h-full px-10">
           <FormSection />
         </div>
       </div>
       <div className=" flex justify-between items-center container mt-20 mb-20">
-        <Image src="./images/image.png" alt="No image" />
+        <Image
+          src="/images/image.png"
+          alt="No image"
+          width={600}
+          height={300}
+        />
         <div className=" w-full h-full px-10">
           <FormSection />
         </div>
       </div>
       <div className=" flex justify-between items-center container mt-20 mb-20">
-        <Image src="./images/image.png" alt="No image" />
+        <Image
+          src="/images/image.png"
+          alt="No image"
+          width={600}
+          height={300}
+        />
         <div className=" w-full h-full px-10">
           <FormSection />
         </div>
